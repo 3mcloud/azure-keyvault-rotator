b007d48adda32d75ca1cab5fa2d20d4757436afa
Shawn Cicoria <github@cicoria.com>
2021-01-13 16:01:20 -0500

    adding test project..

---
+++ tests/KeyVault-Rotation-cs-Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+//https://docs.microsoft.com/en-us/azure/azure-functions/functions-test-a-function#setup
+
+namespace KeyVault_Rotation_cs_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var reqMock = 
+
+        }
+    }
+}
